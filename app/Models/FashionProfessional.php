2cd12206f2fc606828244b92194f99347e455f27
Jorge Fabiano <jorgefkania@gmail.com>
2023-10-26 11:52:20 -0300

    feat: migration fashion_professionals

--- app/Models/FashionProfessional.php
+++ app/Models/FashionProfessional.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FashionProfessional extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasUuids;
 }
