a9bb4c70929e79e385deb4fab1989052ffa965b5
Jorge Fabiano <jorgefkania@gmail.com>
2023-10-26 13:43:47 -0300

    feat: add file readme

--- app/Models/FashionProfessional.php
+++ app/Models/FashionProfessional.php
@@ -10,4 +10,29 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class FashionProfessional extends Model
 {
     use HasFactory, SoftDeletes, HasUuids;
+
+    protected $fillable = [
+        'id',
+        'avatar',
+        'name',
+        'password',
+        'email',
+        'zip_code',
+        'address',
+        'number',
+        'neighborhood',
+        'complement',
+        'city',
+        'long_state',
+        'short_state',
+        'experience',
+        'portifolio_url',
+        'curriculum_url',
+        'time_experience',
+        'prefer_to_work_where',
+        'hiring_regime',
+        'form_of_remuneration',
+        'remuneration_value',
+        'is_active',
+    ];
 }
--- database/migrations/2023_10_26_142918_create_fashion_professionals_table.php
+++ database/migrations/2023_10_26_142918_create_fashion_professionals_table.php
@@ -6,33 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-
-    // - **uuid**: Identificação aberta e única do profissional (tem que criar um observer para gerar o UUID).
-    // - **avatar**: Foto do profissional (opcional)
-    // - **name**: Nome completo do profissional.
-    // - **password**: Senha do profissional.
-    // - **email**: Endereço de e-mail do profissional (unique).
-    // - **zip_code**: Cep do profissional
-    // - **address**: Endereço do profissional
-    // - **number**: Número do endereço do profissional
-    // - **neighborhood**: Bairro do profissional
-    // - **complement**: Complemento do endereço (opcional)
-    // - **city**: Cidade onde o profissional está localizado.
-    // - **state**: Estado ou região onde o profissional está localizado.
-    // - **id_area_of_specialization**: ID relacionado a tabela especialização do profissional (por exemplo, costureiro, estilista, modelista, etc.).
-    // - **experience**: Descrição da experiência e habilidades do profissional.
-    // - **portfolio**: Link para o portfólio online do profissional (opcional).
-    // - **curriculum**: Link para o currículo do profissional (opcional).
-    // - **time_experience**: Tempo de experiencia do profissional (opcional)
-    // - **prefer_to_work_where**: Prefere trabalhar onde (dentro da empresa/diarista/horista/mensal/faccionista)
-    // - **hiring_regime**: Regime de contratação preferido (PJ, CLT, Qualquer um)
-    // - **form_of_remuneration**: Forma de remuneração do profissional, hora/diaria/mês (opcional)
-    // - **remuneration_value**: Valor da remuneração esperada pelo profissional (opcional)
-    // - **is_active**: Ativa ou desativa um registro.
-    // - **created_at**: Data de criação (automático no laravel).
-    // - **updated_at**: Data de atualização (automático no laravel).
-    // - **deleted_at**: Data que foi apagado (automático no laravel com Softdelets).
-
     public function up(): void
     {
         Schema::create('fashion_professionals', function (Blueprint $table) {
@@ -47,17 +20,18 @@ return new class extends Migration
             $table->string('neighborhood');
             $table->string('complement')->nullable();
             $table->string('city');
-            $table->string('name_state');
-            $table->string('acronym_state', 2);
+            $table->string('long_state');
+            $table->string('short_state', 2);
             $table->text('experience');
-            $table->string('portifolio_url');
-            $table->string('curriculum_url');
+            $table->string('portifolio_url')->nullable();
+            $table->string('curriculum_url')->nullable();
             $table->integer('time_experience')->default(0);
-            $table->string('prefer_to_work_where')->default('companies');
+            $table->string('prefer_to_work_where')->default('COMPANIES');
             $table->string('hiring_regime')->default('CLT');
-            $table->string('form_of_remuneration')->default('month');
-            $table->integer('remuneration_value');
+            $table->string('form_of_remuneration')->default('MONTH');
+            $table->integer('remuneration_value')->default(0);
             $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
