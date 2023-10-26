e88716dc0c006d251a6de1d21a690857f4329ff4
Jorge Fabiano <jorgefkania@gmail.com>
2023-10-26 11:31:33 -0300

    feat: model, migration, seed, controller and factor for fashion_professional

--- app/Http/Controllers/FashionProfessionalController.php
+++ app/Http/Controllers/FashionProfessionalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FashionIndustrialMachinesController extends Controller
+class FashionProfessionalController extends Controller
 {
     //
 }
