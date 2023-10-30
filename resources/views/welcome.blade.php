7cc864abb5471e7ddce16227390aa5ceb9aa7a93
Jorge Kania <jorgefkania@gmail.com>
2023-10-30 10:04:05 -0300

    chore: add comment for tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        <!-- COLOAR CONFIGS DO TAILWIND -->
+
 
     </head>
     <body class="antialiased">
