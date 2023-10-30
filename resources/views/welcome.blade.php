afe75ce5a08efcb611f47649c963f70ce9ac3406
Jorge Kania <jorgefkania@gmail.com>
2023-10-30 10:04:39 -0300

    chore: removed comment for tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <!-- COLOAR CONFIGS DO TAILWIND -->
-
-
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
