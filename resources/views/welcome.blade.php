e53501c8eb6a56b53ae9295c04c3edac9941a26e
Jorge Kania <jorgefkania@gmail.com>
2023-10-30 10:38:34 -0300

    feat: tailwindcss installation test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
 
 <body class="antialiased">
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <h1>Teste Docker WSL2</h1>
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-gray-100">
+        <h1 class="text-5xl">Teste Docker WSL2</h1>
     </div>
 </body>
 
