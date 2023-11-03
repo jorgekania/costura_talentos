87c3e6377cc7ccea7a957dfde4ee21123eff2168
Jorge Kania <jorgefkania@gmail.com>
2023-11-03 16:01:56 -0300

    feat: adjustment value in input radio search

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -9,14 +9,18 @@
     <div class="w-4/5 mt-10 bg-white">
         <form action="" class="flex h-14 border rounded-md border-secondary-blue">
             <fieldset class="w-1/4 flex justify-between items-center px-2 border-r border-l-secoborder-secondary-blue">
-                <input id="profissionals" class="peer/profissionals" type="radio" name="status" checked />
-                <label for="profissionals" class="peer-checked/profissionals:text-primary-orange text-secondary-blue">Profissionais</label>
+                <input id="profissionals" class="peer/profissionals" type="radio" name="search-type"
+                    value="profissionals" checked />
+                <label for="profissionals"
+                    class="peer-checked/profissionals:text-primary-orange text-secondary-blue">Profissionais</label>
 
-                <input id="vancancies" class="peer/vancancies" type="radio" name="status" />
-                <label for="vancancies" class="peer-checked/vancancies:text-primary-orange text-secondary-blue">Vagas</label>
+                <input id="vancancies" class="peer/vancancies" type="radio" name="search-type" value="vacancies" />
+                <label for="vancancies"
+                    class="peer-checked/vancancies:text-primary-orange text-secondary-blue">Vagas</label>
 
-              </fieldset>
-            <input type="text" name="search" id="search" placeholder="Buscar Oportunidades" class="w-1/2 py-2 px-3 placeholder:text-secondary-blue placeholder:italic focus:outline-none">
+            </fieldset>
+            <input type="text" name="search" id="search" placeholder="Buscar Oportunidades"
+                class="w-1/2 py-2 px-3 placeholder:text-secondary-blue placeholder:italic focus:outline-none">
             <button class="w-1/4 py-2 px-3 bg-primary-orange hover:bg-hover-orange text-white">Buscar</button>
         </form>
     </div>
