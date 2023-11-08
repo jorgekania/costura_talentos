c3704375ce2795de22609e73f7880fd6f62412c8
Jorge Kania <jorgefkania@gmail.com>
2023-11-08 09:05:06 -0300

    feat: modified faker in FashionVacancies

--- database/factories/FashionVacancyFactory.php
+++ database/factories/FashionVacancyFactory.php
@@ -18,8 +18,8 @@ class FashionVacancyFactory extends Factory
             'title' => fake()->sentence(3),
             'fashion_company_id'              => FashionCompany::all()->random()->id,
             'specializations_id'              => FashionProfessionalSpecialization::all()->random()->id,
-            'activities_and_responsibilities' => fake()->sentence(5),
-            'vacancy_requirements'            => fake()->sentence(5),
+            'activities_and_responsibilities' => fake()->realText(500, 3),
+            'vacancy_requirements'            => fake()->realText(500, 5),
             'work_where' => FormOfRemuneration::getRandomEnumValue(),
             'hiring_regime' => HiringRegime::getRandomEnumValue()
         ];
